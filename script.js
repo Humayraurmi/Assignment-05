74d03392f8e166041642632e045499c95a933b3a
Humayraurmi <humayraurmi25@gmail.com>
2025-08-28 16:32:55 +0600

    fifth commit

--- script.js
+++ script.js
@@ -10,20 +10,45 @@ for (let i = 0; i < hearts.length; i++) {
 }
 
 // call button
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let coins = 100;
     const coinSpan = document.querySelector("#coin-btn-count");
     const callButtons = document.querySelectorAll(".cll-btn");
+    const callHistoryList = document.querySelector("#call-history");
+    const clearBtn = document.querySelector("#clear-history");
 
     for (let i = 0; i < callButtons.length; i++) {
-        callButtons[i].addEventListener("click", function() {
-            coins -= 20;
+        callButtons[i].addEventListener("click", function (e) {
             if (coins < 20) {
-                alert("Not enough coint to make a call");
+                alert("Not enough coins to make a call");
+                return;
             }
+
+            coins -= 20;
             coinSpan.textContent = coins;
+
+            const card = e.target.closest(".p-5");
+            const serviceName = card.querySelector("h4").innerText;
+            const serviceNumber = card.querySelector("h2").innerText;
+            alert(`Calling ${serviceName} (${serviceNumber})`);
+            const timeNow = new Date();
+            const timeStr = timeNow.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+            const li = document.createElement("li");
+            li.className = "flex justify-between items-center bg-gray-100 px-3 py-2 rounded-lg shadow-sm";
+            li.innerHTML = `
+                <div>
+                    <p class="font-medium">${serviceName}</p>
+                    <p class="text-xs text-gray-500">${serviceNumber}</p>
+                </div>
+                <span class="text-xs text-gray-400">${timeStr}</span>
+            `;
+            callHistoryList.prepend(li);
         });
     }
-});
+    clearBtn.addEventListener("click", function () {
+        callHistoryList.innerHTML = "";
+    })
+})
+
 
 
